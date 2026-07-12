0fab9dd7e6fc7718a3751a46034ac55e9ab3b78c
geromguershom-dot <geromguershom@gmail.com>
2026-07-12 17:54:30 +1100

    Refactor import statement in ClosingCTA component

--- src/components/ClosingCTA.jsx
+++ src/components/ClosingCTA.jsx
@@ -1,6 +1,4 @@
-
-         
-               import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Clock, ShieldCheck, MessageSquare } from "lucide-react";
 
 const points = [
